7bb9cab9164c21e4a0ed2687d36dfd6fde54d366
Doug Jih <doug.jih@gmail.com>
2020-11-28 21:34:14 -0500

    Add toy test as initial demo of using Firefox WebDriver
    This test loads the web site project login page and tries to log in.

---
+++ Tests/Tests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace WaterAmericaMoveSite
+{
+    class Tests
+    {
+        IWebDriver webDriver;
+
+        [SetUp]
+        public void start_Browser()
+        {
+            webDriver = new FirefoxDriver();
+        }
+
+        [TearDown]
+        public void close_Browser()
+        {
+            webDriver.Close();
+        }
+
+        [Test]
+        public void LoadLoginPageAndLogin()
+        {
+            webDriver.Url = "http://localhost/Water-America-Project/login.php";
+
+            {
+                var webElement = webDriver.FindElement(By.XPath("//input[@name='username']"));
+                webElement.SendKeys("customerlogin");
+            }
+            {
+                var webElement = webDriver.FindElement(By.XPath("//input[@name='password']"));
+                webElement.SendKeys("customerpassword");
+            }
+            {
+                var webElement = webDriver.FindElement(By.XPath("//input[@value='Login']"));
+                webElement.Click();
+            }
+        }
+    }
+}
